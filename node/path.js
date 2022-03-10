227fa08927e3188d151ee09610154c123958a13c
BHARATH KUMAR NETRAMBAKAM <bharathdarling970070@gmail.com>
2022-03-10 22:12:14 +0530

    path.js

---
+++ node/path.js
@@ -0,0 +1,16 @@
+// "E:\FJP6\node\os.js"
+
+let path = require("path")
+
+// console.log(path) //Tells us About path object
+
+// let extensionName = path.extname("E:\FJP6\node\path.js");
+// console.log(extensionName) //prints extensionName of any file path
+
+// let Base_Name = path.basename("E:\FJP6\node\path.js")
+// \node is taking as \n ode new line ode -- Some Error in Base Name
+// console.log(Base_Name) //Prints Name of the given file path
+
+console.log(__dirname) //Prints The path of the Directory
+
+console.log(__filename) //Prints The path of the File
