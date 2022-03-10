4d0797621d3ca31d0642c116ab77058210af8181
BHARATH KUMAR NETRAMBAKAM <bharathdarling970070@gmail.com>
2022-03-10 23:25:37 +0530

    appendFileSync in fs

--- node/file.js
+++ node/file.js
@@ -1,1 +1,2 @@
 console.log("Content of file.js is over written by fs.js")
+Added Extra Content at the end of the file of given file path
--- node/fs.js
+++ node/fs.js
@@ -3,7 +3,9 @@ let fs = require("fs")
 
 let path = require("path")
 let filepath = path.join(__dirname,"file.js")
+//CRUD OPERATIONS
 
+//-->Create
 fs.writeFileSync(filepath,"console.log(\"Hello I am text file generated from fs.js\")")
 
 let cp = require("child_process")
@@ -14,5 +16,15 @@ console.log(""+ cp.execSync("node file.js"))
 fs.writeFileSync(filepath,"console.log(\"Content of file.js is over written by fs.js\")")
 console.log(""+ cp.execSync("node file.js"))
 
+//-->Read
 let content = fs.readFileSync(filepath,"utf-8")
+// console.log(content) //Prints content in Readable form
+
+// content = fs.readFileSync(filepath)
+// console.log(content) //Prints content in Binary form[Buffered]
+
+//-->update -- Append
+fs.appendFileSync(filepath,"\nAdded Extra Content at the end of the file of given file path")
+content = fs.readFileSync(filepath,"utf-8")
 console.log(content)
+
